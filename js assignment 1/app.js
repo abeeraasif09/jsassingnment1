623c7811b9b3e9899ef155726d8a8fb64624777d
Abeera Asif <145438487+abeeraasif09@users.noreply.github.com>
2023-10-06 17:38:03 +0500

    Add files via upload

---
+++ js assignment 1/app.js
@@ -0,0 +1,12 @@
+// exercise 1.3
+
+// 1. Create a separate file called app with the extension .js.
+console.log("hello world app");
+
+// exercise 1.4
+
+// Set a variable 'a' to the value 10
+let a = 10; 
+console.log("The value of 'a' is:", a);
+// The line above logs the value of the variable 'a' to the console.
+
---
+++ js assignment 1/script.js
@@ -0,0 +1,33 @@
+// example 1.1 question 1
+
+// 1. Open the browser console, type 4 + 10, and press Enter. What do you see as
+// the response?
+
+var result = 4 + 10;
+console.log(result);
+
+
+// example 1.1 question 2
+
+// 2. Use the console.log() syntax, placing a value within the rounded brackets.
+// Try entering your name with quotes around it (this is to indicate the fact that
+// it's a text string—we'll get to this in the next chapter).
+
+var yourName = "Abeera";
+console.log(yourName);"Your Name"
+
+// example 1.2 
+
+console.log("Hello, world!");
+
+// Self-check quiz
+// answer 1
+
+
+
+
+
+
+
+
+
